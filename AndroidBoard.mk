2cb1fa8ad202a440ca35e1d3387510357caf6318
UISS <uiss520@gmail.com>
2013-04-26 23:45:56 +0800

    Fix the buildding error

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,8 +4,5 @@ include $(CLEAR_VARS)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
-# copy to obj/ the prebuilts needed to build open source libraries
--include device/motorola/qinara/prebuilt.mk
-
 # include the non-open-source counterpart to this file
 -include vendor/motorola/qinara/AndroidBoardVendor.mk
