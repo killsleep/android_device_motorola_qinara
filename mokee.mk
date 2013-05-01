9ce332fa2b33d1d73a0c3c785b4d8bd69e8e1bc9
zerojim <zerojim520@gmail.com>
2013-05-01 16:50:30 +0800

    add the cos.mk and mokee.mk file

---
+++ mokee.mk
@@ -0,0 +1,35 @@
+# Inherit device configuration for Motorola Atrix HD.
+$(call inherit-product, device/motorola/qinara/full_qinara.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := mk_qinara
+PRODUCT_BRAND := motorola
+PRODUCT_DEVICE := qinara
+PRODUCT_MODEL := MB886
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_RELEASE_NAME := motorola Atrix HD
+PRODUCT_SFX := cn
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+UTC_DATE := $(shell date +%s)
+DATE := $(shell date +%Y%m%d)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=${PRODUCT_MODEL}_${PRODUCT_SFX} \
+   BUILD_NUMBER=${DATE} \
+   TARGET_DEVICE=${PRODUCT_DEVICE} \
+   BUILD_FINGERPRINT=${PRODUCT_BRAND}/${PRODUCT_MODEL}_${PRODUCT_SFX}/${PRODUCT_DEVICE}:${PLATFORM_VERSION}/${BUILD_ID}/${DATE}:user/release-keys \
+   PRIVATE_BUILD_DESC="${PRODUCT_DEVICE}-user ${PLATFORM_VERSION} ${BUILD_ID} ${DATE} release-keys" \
+   PRODUCT_BRAND=${PRODUCT_BRAND} \
+   BUILD_UTC_DATE=
